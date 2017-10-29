0a04cc6fb294cc8c28f1bf20a9103982ac8c459e
Karnabanu <karnabanuk123@gmail.com>
2017-10-29 17:01:26 +0530

    Update guessinggame.sh
    fuctipn added

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,19 +1,21 @@
-echo "How many files are there"
 var=$(ls -l . | egrep -c '^-')
+function getInputs(){
+	echo "How many files are there"
+	read n
+}
 while true
 do
-    read n
+	getInputs
     if  ! [[ $n =~ ^[0-9]+$ ]]
     then
 	echo "Only numbers are allowed"
-	echo  "How many files are there"
     elif [[ n -eq var ]]
     then
 	echo "Congratulations your guess is correct"
-	exit
+	exit 0
     elif [[ n -gt var ]]
     then
-	 echo "Guess is too high"
+	echo "Guess is too high"
     else
 	echo "Guess is too small"
     fi
